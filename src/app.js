e537f73505bc6898862b75870b23fd71c4ba71f9
Powarox219 <moriniere.robin1877@gmail.com>
2022-02-01 21:06:10 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -5,17 +5,11 @@ import { fetchDataBase, updateWallet } from './services/airtable.js'
 
 const server = http.createServer((req, res) => {
 
-    /*
-    * Fetch crypto price with coinGecko
-    *
-    * @result list crypto price
-    */
-    let data = fetchPrice()
-    data.then((price) => {
-        // console.log(price)
+    fetchPrice().then((price) => {
+        console.log(price)
     })
 
     res.end('coucou')
 })
 
-server.listen(process.env.PORT) // process.env.PORT ||
+server.listen(process.env.PORT)
