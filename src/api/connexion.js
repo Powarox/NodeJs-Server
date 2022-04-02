03ec5b062f93bf9dd8b2768b7875ae50d7a40d9a
@RobinSinay <robin.moriniere@sinay.fr>
2022-04-02 18:02:35 +0200

    Change Database to TestBase

--- src/api/connexion.js
+++ src/api/connexion.js
@@ -8,9 +8,14 @@ import Airtable from "airtable"
 //     apiKey: process.env.AIRTABLE_SECRET_KEY
 // }).base(process.env.AIRTABLE_REAL_SECRET_BASE)
 
+// const airtableTestBase = new Airtable({
+//     apiKey: process.env.AIRTABLE_SECRET_KEY
+// }).base(process.env.AIRTABLE_TEST_SECRET_BASE)
+
 const airtableBase = new Airtable({
     apiKey: process.env.AIRTABLE_SECRET_KEY
-}).base(process.env.AIRTABLE_DEV_SECRET_BASE)
+}).base(process.env.AIRTABLE_TEST_SECRET_BASE)
+
 
 export {
     airtableBase
--- src/config/index.js
+++ src/config/index.js
@@ -6,5 +6,6 @@ export default {
         secretKey: process.env.AIRTABLE_SECRET_KEY,
         secretDevBase: process.env.AIRTABLE_DEV_SECRET_BASE,
         secretRealBase: process.env.AIRTABLE_REAL_SECRET_BASE,
+        secretTestBase: process.env.AIRTABLE_TEST_SECRET_BASE,
     },
 }
