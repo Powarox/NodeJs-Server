fc1116d6ffaf34da89f2ff5739d42dc0adf37253
Powarox2159 <moriniere.robin1877@gmail.com>
2022-05-26 23:00:13 +0200

    feat: clean code

--- src/app.js
+++ src/app.js
@@ -5,68 +5,62 @@ import { fetchWalletDataBase, fetchCoinsListDataBase } from './services/airtable
 import { updateWallet, updateCoinsList, createReccords } from './services/airtable.js';
 
 const server = http.createServer((req, res) => {
-    console.log('Server is working...');
+    console.log("Server is working...");
 
-    function updatePriceAirtable() {
-        console.log('Start to update price...');
+    function updateWalletPriceAirtable() {
+        console.log("Start to update wallet price...");
         fetchWalletDataBase().then((data) => {
             fetchPrice().then((price) => {
-                // for(let id in data) {
-                //     for(let j in price.data) {
-                //         if(data[id].PriceName === j) {
-                //             data[id].MarketPrice = price.data[j].usd
-                //             updateWallet(data[id], id)
-                //         }
-                //     }
-                // }
-                console.log(data);
-                console.log(price);
+                for(let id in data) {
+                    for(let j in price.data) {
+                        if(data[id].PriceName === j) {
+                            data[id].MarketPrice = price.data[j].usd;
+                            updateWallet(data[id], id);
+                        }
+                    }
+                }
                 console.log("Update finish !");
             });
         });
     }
 
-    function updatecoinsListPriceAirtable(){
-        console.log('Start to update coins list price...');
+    function updatecoinsListPriceAirtable() {
+        console.log("Start to update coins list price...");
         fetchCoinsListDataBase().then((data) => {
             fetchPrice().then((price) => {
-                console.log(data);
-                for(let id in data) {
-                    for(let j in price.data) {
-                        if(data[id].CoingeckoID === j) {
-                            data[id].MarketPrice = price.data[j].usd
+                for (let id in data) {
+                    for (let j in price.data) {
+                        if (data[id].CoingeckoID === j) {
+                            data[id].MarketPrice = price.data[j].usd;
                             updateCoinsList(data[id], id);
                         }
                     }
                 }
-            })
-        })
+            });
+        });
     }
-
-    updatecoinsListPriceAirtable()
-
-    // updatePriceAirtable();
 
     function createReccordAirtable() {
-        console.log('Create new reccord of total value...');
-        let totalAmounts = 0
-        let totalMarketValue = 0
-        let totalTakeProfits = 0
+        console.log("Create new reccord of total value...");
+        let totalAmounts = 0;
+        let totalMarketValue = 0;
+        let totalTakeProfits = 0;
 
-        fetchDataBase().then((data) => {
-            for(let id in data) {
-                totalAmounts += data[id].Amounts
-                totalTakeProfits += data[id].Selled
-                totalMarketValue += data[id].MarketPrice
+        fetchWalletDataBase().then((data) => {
+            for (let id in data) {
+                totalAmounts += data[id].Amounts;
+                totalTakeProfits += data[id].Selled;
+                totalMarketValue += data[id].MarketPrice;
             }
-            createReccords(totalAmounts, totalTakeProfits, totalMarketValue)
-        })
+            createReccords(totalAmounts, totalTakeProfits, totalMarketValue);
+        });
     }
 
-    // setInterval(updatePriceAirtable, 1000*60*15) 
-    // setInterval(createReccordAirtable, 1000*60*60*24)  
+    setInterval(updateWalletPriceAirtable, 1000*60*15);
+    setInterval(updatecoinsListPriceAirtable, 1000*60*15);
+    setInterval(createReccordAirtable, 1000*60*60*24);
 
-    res.end('NodeJs server is runing !')
+    res.end("NodeJs server is runing !");
 })
 
-server.listen(process.env.PORT)
+server.listen(process.env.PORT);
