8ed303cc1a44cce14b20b6d8c86c2bdf561f591f
Powarox2159 <moriniere.robin1877@gmail.com>
2022-05-31 22:42:47 +0200

    feat: tests to fix deploy on heroku

--- src/services/node.js
+++ src/services/node.js
@@ -1,19 +1,13 @@
-function terminate(server, options = { coredump: false, timeout: 500 }) {
-    // Exit function
+export function terminate(server, options = { coredump: false, timeout: 500 }) {
     const exit = (code) => {
         options.coredump ? process.abort() : process.exit(code);
     };
 
     return (code, reason) => (err, promise) => {
         if (err && err instanceof Error) {
-            // Log error information, use a proper logging library here :)
             console.log(err.message, err.stack);
         }
-
-        // Attempt a graceful shutdown
         server.close(exit);
         setTimeout(exit, options.timeout).unref();
     };
 }
-
-module.exports = terminate;
