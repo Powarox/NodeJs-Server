645787ea33c15241aadb2bb758e0de3b306aac16
Powarox <moriniere.robin1877@gmail.com>
2022-06-19 23:20:51 +0200

    fix: helper 401 errors

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,5 +5,5 @@ export function authorization(req, res) {
     if (req.headers.authorization === process.env.REST_API_KEY) {
         return true;
     }
-    return response.error(res, "Access denied");
+    return response.unauthorizedResponse(res, "Access denied");
 }
